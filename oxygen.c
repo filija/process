2c984895ff986acbc18dc01d075df9d3678d7d3c
filija <filija.jakub@gmail.com>
2015-04-19 18:35:58 +0200

    start algorhytm

--- oxygen.c
+++ oxygen.c
@@ -1,7 +1,30 @@
 
 #include "aqua.h"
 
-void testOxygen()
+void Oxygen(TSynchWater synch, int *sharedMemory, int maxGo, int i)
 {
-	printf("Oxygen funguje\n");
+	srand(time(NULL));
+
+	sem_wait(synch.mutex); //mutex wait
+	synch.oxygen++;
+
+	if(synch.hydrogen>=2)
+	{
+		printf("oxygen if\n");
+		sem_post(synch.hydroQueue);
+		sem_post(synch.hydroQueue);
+		synch.hydrogen=synch.hydrogen-2;
+		sem_post(synch.oxyQueue);
+		synch.oxygen--;
+	}		
+
+	else{
+		printf("oxygen else\n");
+		sem_post(synch.mutex);
+	}
+
+	sem_wait(synch.oxyQueue);
+	//bond();
+	sem_post(synch.mutex);
+
 }
