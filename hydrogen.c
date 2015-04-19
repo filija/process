180b7c9ed61435be12417c0a041d930c698def37
filija <filija.jakub@gmail.com>
2015-04-19 18:35:36 +0200

    start algorhytm

--- hydrogen.c
+++ hydrogen.c
@@ -1,7 +1,34 @@
 
 #include "aqua.h"
 
-void testHydrogen()
+void Hydrogen(TSynchWater synch, int *sharedMemory, int maxGh, int i)
 {
-	printf("hydrogen funguje\n");
+	if(sharedMemory[1]==1)
+	{
+		printf("%d		:H %d	: started", sharedMemory[1], synch.hydrogen);
+		sharedMemory[1]++;
+	}
+
+	srand(time(NULL));
+
+		sem_wait(synch.mutex);
+		synch.hydrogen++;
+		if(synch.hydrogen>=2 && synch.oxygen>=1)
+		{	
+			printf("hydrogen if\n");
+			sem_post(synch.hydroQueue);
+			sem_post(synch.hydroQueue);
+			synch.hydrogen=synch.hydrogen-2;
+			sem_post(synch.oxyQueue);
+			synch.oxygen--;
+		}
+
+		else{
+			printf("hydrogen else\n");
+			sem_post(synch.mutex);
+		}
+
+		sem_wait(synch.hydroQueue);
+		//bond();
+
 }
