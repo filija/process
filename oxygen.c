c9fba155f7a93a42063e5db005602d2078edd12f
filija <filija.jakub@gmail.com>
2015-04-21 15:19:35 +0200

    new edit

--- oxygen.c
+++ oxygen.c
@@ -3,28 +3,5 @@
 
 void Oxygen(TSynchWater synch, int *sharedMemory, int maxGo, int i)
 {
-	srand(time(NULL));
-
-	sem_wait(synch.mutex); //mutex wait
-	synch.oxygen++;
-
-	if(synch.hydrogen>=2)
-	{
-		printf("oxygen if\n");
-		sem_post(synch.hydroQueue);
-		sem_post(synch.hydroQueue);
-		synch.hydrogen=synch.hydrogen-2;
-		sem_post(synch.oxyQueue);
-		synch.oxygen--;
-	}		
-
-	else{
-		printf("oxygen else\n");
-		sem_post(synch.mutex);
-	}
-
-	sem_wait(synch.oxyQueue);
-	//bond();
-	sem_post(synch.mutex);
-
+	printf("%d:\tO %d:\tstarted.\n", sharedMemory[1]++, sharedMemory[3]++);		
 }
