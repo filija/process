369cf460dfe34dae80bbd5a49ec20396677abe26
filija <filija.jakub@gmail.com>
2015-04-23 15:29:31 +0200

    some edit

--- oxygen.c
+++ oxygen.c
@@ -1,7 +1,31 @@
 
 #include "aqua.h"
 
-void Oxygen(TSynchWater synch, int *sharedMemory, int maxGo, int i)
-{
-	printf("%d:\tO %d:\tstarted.\n", sharedMemory[1]++, sharedMemory[3]++);		
+void Oxygen(TSynchWater synch, int *sharedMemory, TParams param, int i)
+{	
+	srand(time(NULL));
+	sem_wait(synch.mutex);
+	sharedMemory[3]++;
+	printf("%d:\tO %d:\tstarted.\n", sharedMemory[1]++, i);
+	printf("%d:\tO %d:\twaiting.\n", sharedMemory[1]++, i);
+	if(sharedMemory[2]>=2)
+	{
+		sem_post(synch.hydroQueue);
+		sem_post(synch.hydroQueue);
+		sharedMemory[2]=sharedMemory[2]-2;
+		sem_post(synch.oxyQueue);
+		sharedMemory[3]--;
+	}
+
+	else{
+		sem_post(synch.mutex);
+	}
+
+	sem_wait(synch.oxyQueue);
+
+	printf("%d:\tO %d:\tbegin bonding.\n", sharedMemory[1]++, i);
+	sleep(rand()%(param.B)/1000);
+	printf("%d:\tO %d:\tbonded.\n", sharedMemory[1]++, i);
+	sem_post(synch.mutex);
+
 }
