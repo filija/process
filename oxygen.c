d7f2807a91e8e390a4456797753315707c18db77
filija <filija.jakub@gmail.com>
2015-04-30 10:22:36 +0200

    zaloha

--- oxygen.c
+++ oxygen.c
@@ -3,29 +3,51 @@
 
 void Oxygen(TSynchWater synch, int *sharedMemory, TParams param, int i)
 {	
+
 	srand(time(NULL));
 	sem_wait(synch.mutex);
+	
+	if(param.GO!=0)
+	{		
+		sleep((rand()%param.GO)/1000);
+	}
+	
 	sharedMemory[3]++;
-	printf("%d:\tO %d:\tstarted.\n", sharedMemory[1]++, i);
-	printf("%d:\tO %d:\twaiting.\n", sharedMemory[1]++, i);
+	sharedMemory[4]++;
+	
+	printf("%d:\tO %d:\tstarted.\n", sharedMemory[1]++, sharedMemory[4]);
+	
+
 	if(sharedMemory[2]>=2)
 	{
 		sem_post(synch.hydroQueue);
 		sem_post(synch.hydroQueue);
+
+		printf("%d:\tO %d:\tready\n", sharedMemory[1]++, sharedMemory[4]);
+
 		sharedMemory[2]=sharedMemory[2]-2;
 		sem_post(synch.oxyQueue);
 		sharedMemory[3]--;
 	}
 
 	else{
+		
+		printf("%d:\tO %d:\twaiting\n", sharedMemory[1]++, sharedMemory[4]);
+		
 		sem_post(synch.mutex);
 	}
 
 	sem_wait(synch.oxyQueue);
 
-	printf("%d:\tO %d:\tbegin bonding.\n", sharedMemory[1]++, i);
-	sleep(rand()%(param.B)/1000);
-	printf("%d:\tO %d:\tbonded.\n", sharedMemory[1]++, i);
+	printf("%d:\tO %d:\tbegin bonding\n", sharedMemory[1]++, sharedMemory[4]);	
+
+	if(param.B!=0)
+	{
+		sleep((rand()%param.B)/1000);
+	}
+
+	printf("%d:\tO %d:\tbonded\n", sharedMemory[1]++, sharedMemory[4]);
+
 	sem_post(synch.mutex);
 
 }
