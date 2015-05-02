9b52ef367290c14787b25eda1fe05447b374b7c3
filija <filija.jakub@gmail.com>
2015-05-02 10:09:12 +0200

    adasd

--- oxygen.c
+++ oxygen.c
@@ -2,28 +2,29 @@
 #include "aqua.h"
 
 void Oxygen(TSynchWater synch, int *sharedMemory, TParams param, int i)
-{	
-
+{
+	int c_hydro=0;
 	srand(time(NULL));
 	sem_wait(synch.mutex);
-	
 	if(param.GO!=0)
 	{		
 		sleep((rand()%param.GO)/1000);
 	}
-	
+		
 	sharedMemory[3]++;
 	sharedMemory[4]++;
-	
-	printf("%d:\tO %d:\tstarted.\n", sharedMemory[1]++, sharedMemory[4]);
-	
+	sem_wait(synch.locker);
+	fprintf(synch.outFile, "%d:\tO %d:\tstarted.\n", sharedMemory[1]++, sharedMemory[4]);
+	sem_post(synch.locker);
 
 	if(sharedMemory[2]>=2)
 	{
 		sem_post(synch.hydroQueue);
 		sem_post(synch.hydroQueue);
 
-		printf("%d:\tO %d:\tready\n", sharedMemory[1]++, sharedMemory[4]);
+		sem_wait(synch.locker);
+		fprintf(synch.outFile, "%d:\tO %d:\tready\n", sharedMemory[1]++, sharedMemory[4]);
+		sem_post(synch.locker);
 
 		sharedMemory[2]=sharedMemory[2]-2;
 		sem_post(synch.oxyQueue);
@@ -31,22 +32,27 @@ void Oxygen(TSynchWater synch, int *sharedMemory, TParams param, int i)
 	}
 
 	else{
-		
-		printf("%d:\tO %d:\twaiting\n", sharedMemory[1]++, sharedMemory[4]);
-		
+		sem_wait(synch.locker);
+		fprintf(synch.outFile, "%d:\tO %d:\twaiting\n", sharedMemory[1]++, sharedMemory[4]);
+		sem_post(synch.locker);
 		sem_post(synch.mutex);
 	}
 
 	sem_wait(synch.oxyQueue);
 
-	printf("%d:\tO %d:\tbegin bonding\n", sharedMemory[1]++, sharedMemory[4]);	
+	sem_wait(synch.locker);
+	fprintf(synch.outFile, "%d:\tO %d:\tbegin bonding\n", sharedMemory[1]++, sharedMemory[4]);	
+	c_hydro++;
+	sem_post(synch.locker);
 
 	if(param.B!=0)
 	{
 		sleep((rand()%param.B)/1000);
 	}
 
-	printf("%d:\tO %d:\tbonded\n", sharedMemory[1]++, sharedMemory[4]);
+	sem_wait(synch.locker);
+	fprintf(synch.outFile, "%d:\tO %d:\tbonded\n", sharedMemory[1]++, sharedMemory[4]);
+	sem_post(synch.locker);
 
 	sem_post(synch.mutex);
 
