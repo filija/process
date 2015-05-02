469bbd34e1edbfa036ddbdf56083a2aedfbe09e9
filija <filija.jakub@gmail.com>
2015-05-02 10:09:02 +0200

    adsadsa

--- hydrogen.c
+++ hydrogen.c
@@ -3,24 +3,28 @@
 
 void Hydrogen(TSynchWater synch, int *sharedMemory, TParams param, int i)
 {
-
+	int c_hydro=0;
 	srand(time(NULL));
 	sem_wait(synch.mutex);	
 	if(param.GH!=0)
 	{		
 		sleep((rand()%param.GH)/1000);	
 	}
+	
 	sharedMemory[2]++;
 	sharedMemory[0]++;
 
-	printf("%d:\tH %d:\tstarted.\n", sharedMemory[1]++, sharedMemory[0]);
+	sem_wait(synch.locker);
+	fprintf(synch.outFile, "%d:\tH %d:\tstarted.\n", sharedMemory[1]++, sharedMemory[0]);
+	sem_post(synch.locker);
 
 	if(sharedMemory[2]>=2 && sharedMemory[3]>=1)
 	{		
+		sem_wait(synch.locker);
+		fprintf(synch.outFile, "%d:\tH %d:\tready.\n", sharedMemory[1]++, sharedMemory[0]);
+		sem_post(synch.locker);
 		sem_post(synch.hydroQueue);
 		sem_post(synch.hydroQueue);
-
-		printf("%d:\tH %d:\tready.\n", sharedMemory[1]++, sharedMemory[0]);
 
 		sharedMemory[2]=sharedMemory[2]-2;
 		sem_post(synch.oxyQueue);
@@ -28,22 +32,26 @@ void Hydrogen(TSynchWater synch, int *sharedMemory, TParams param, int i)
 	}
 
 	else{
-
-		printf("%d:\tH %d:\twaiting.\n", sharedMemory[1]++, sharedMemory[0]);
-
+		sem_wait(synch.locker);
+		fprintf(synch.outFile, "%d:\tH %d:\twaiting.\n", sharedMemory[1]++, sharedMemory[0]);
+		sem_post(synch.locker);
 		sem_post(synch.mutex);
 	}
 
 	 sem_wait(synch.hydroQueue);
-	
-	printf("%d:\tH %d:\tbegin bonding.\n", sharedMemory[1]++, sharedMemory[0]);
-	
+
+	sem_wait(synch.locker);
+	fprintf(synch.outFile, "%d:\tH %d:\tbegin bonding.\n", sharedMemory[1]++, sharedMemory[0]);
+	c_hydro++;
+	sem_post(synch.locker);
 
 	if(param.B!=0)
 	{
 		sleep((rand()%param.B)/1000);
 	}
 
-	printf("%d:\tH %d:\tbonded.\n", sharedMemory[1]++, sharedMemory[0]);
-	
+	sem_wait(synch.locker);
+	fprintf(synch.outFile, "%d:\tH %d:\tbonded\n", sharedMemory[1]++, sharedMemory[0]);
+	sem_post(synch.locker);
+
 }
